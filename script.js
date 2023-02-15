e5739000e8c74c7a161d898f4a5ad77eeab4e630
Leonardo Alves <leonardo.alves779@gmail.com>
2023-02-14 21:56:32 -0300

    creating a Readme

---
+++ script.js
@@ -0,0 +1,7 @@
+// - Pegar o click de cada tecla
+//  -> separar teclas em números e sinais operadores
+
+// - imprimir o valor de cada tecla no console.log e na div superior também
+
+// - Fazer o cursor ficar piscando
+
