ed6917da8e5b7a571b00054f296941bf81b0d335
Leonardo Alves <leonardo.alves779@gmail.com>
2023-02-21 12:55:37 -0300

    finishing the operations

--- script.js
+++ script.js
@@ -63,9 +63,10 @@ operators.forEach(operator => operator.addEventListener('click', selectOperator)
 
 
 // Using equal operator to show the result
-const callEqual = () => {
-    calculate //``` BUG HERE: i cannot continue the operation after click the iqual button ```
+const callEqual = () => { 
+    calculate
     operator = undefined
+    newNumber = false
 }
 
 document.getElementById('equals-operator').addEventListener('click', callEqual)
@@ -117,5 +118,51 @@ const insertDecimal = () => {
 document.getElementById('comma').addEventListener('click', insertDecimal)
 
 
+// Pi button
+const addPiOnDisplay = () => {
+    calculate
+    newNumber = false
+    display.textContent = 3.14
+    currentNumber = 3.14
+} 
 
-// (video:59min)
+document.getElementById('pi-button').addEventListener('click', addPiOnDisplay)
+
+
+// Mapping keyboard clicks
+const keyboardMap = {
+    '0' : '0-button',
+    '1' : '1-button',
+    '2' : '2-button',
+    '3' : '3-button',
+    '4' : '4-button',
+    '5' : '5-button',
+    '6' : '6-button',
+    '7' : '7-button',
+    '8' : '8-button',
+    '9' : '9-button',
+    '/' : 'divide-operator',
+    '*' : 'multiply-operator',
+    '-' : 'minus-operator',
+    '+' : 'more-operator',
+    '=' : 'equals-operator',
+    'c' : 'clear',
+    'C' : 'clear',
+    ',' : 'comma',
+    'p' : 'pi-button',
+    'P' : 'pi-button',
+    'Enter' : 'equals-operator',
+    'Backspace' : 'backspace'
+}
+
+const mappingKeyboard = (event) => {
+    const key = event.key
+
+    const allowedKey = () => Object.keys(keyboardMap).indexOf(key) !== -1
+    
+    if(allowedKey()){
+        document.getElementById(keyboardMap[key]).click()
+    }    
+}
+
+document.addEventListener('keydown', mappingKeyboard)
